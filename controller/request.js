687c880241661c33561791c9559c7d95851259f3
himotechglobal <info@himotechglobal.com>
2023-11-01 21:08:29 +0530

    test

--- controller/request.js
+++ controller/request.js
@@ -346,7 +346,7 @@ async function sendNextMsg(bot,chatId,msg,network,requestId){
     try{
         if(msg == "ROUTER_ADDRESS"){
       
-            bot.sendMessage(chatId, "Please choose a router for the token liquidity" , routerMarkup[network]);            
+            bot.sendMessage(chatId, "Please choose a router for the token liquidity"+network , routerMarkup[network]);            
         } 
         
         else if(msg == "TRADING"){      
